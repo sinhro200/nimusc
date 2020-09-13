835e797eff647e4fec62ab9ed7a1320287f076ae
sinhro200 <sinhro-200@mail.ru>
2020-09-13 22:10:07 +0300

    Добавил статичный конструктор, и переименовал класс

--- src/main/java/nimusc/core/common/CommonMapParameters.java
+++ src/main/java/nimusc/core/common/CommonMapParameters.java
@@ -1,6 +1,7 @@
 package nimusc.core.common;
 
-import nimusc.core.request.RequestHeaderParams;
+import nimusc.core.common.exception.NimuscException;
+import nimusc.core.request.RequestHeaderParameters;
 import nimusc.core.request.HttpUrlParameters;
 import okhttp3.HttpUrl;
 import okhttp3.Request;
@@ -8,19 +9,24 @@ import okhttp3.Request;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CommonMapParameters implements RequestHeaderParams, HttpUrlParameters {
+public class CommonMapParameters implements RequestHeaderParameters, HttpUrlParameters {
     private Map<String,String> map;
 
     public CommonMapParameters() {
         map = new HashMap<>();
     }
 
-    public void addParam(String key,String value){
+    public static CommonMapParameters createEmpty(){
+        return new CommonMapParameters();
+    }
+
+    public CommonMapParameters addParam(String key,String value){
         map.put(key,value);
+        return this;
     }
 
     @Override
-    public void applyToHttpBuilder(HttpUrl.Builder urlBuilder) throws CommonException{
+    public void applyToHttpBuilder(HttpUrl.Builder urlBuilder) throws NimuscException {
         map.forEach(
                 (k,v)->{
                     urlBuilder.addQueryParameter(k,v);
@@ -29,7 +35,7 @@ public class CommonMapParameters implements RequestHeaderParams, HttpUrlParamete
     }
 
     @Override
-    public void applyToRequestBuilder(Request.Builder requestBuilder) throws CommonException {
+    public void applyToRequestBuilder(Request.Builder requestBuilder) throws NimuscException {
         map.forEach(
                 (k,v)->{
                     requestBuilder.header(k,v);
